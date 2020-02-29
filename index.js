22a4055d106f92f45e49af73e3b3b72db8339287
braeden123 <braedensmith2@gmail.com>
2020-02-29 00:38:48 -0600

    starting to come together

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const xmlParser = require('./parser')
 const app = express()
 const port = process.env.PORT || 3000
 app.use(express.static('public'))
@@ -6,47 +7,91 @@ const fileUpload = require('express-fileupload');
 app.use(fileUpload());
 const language = require('@google-cloud/language');
 const client = new language.LanguageServiceClient();
-const parser = require('fast-xml-parser');
 
 
 
-const text = 'I hate you tyler! I love you tyler.';
+// const text = 'I hate you tyler! I love you tyler.';
 
-const document = {
-    content: text,
-    type: 'PLAIN_TEXT',
-};
+// const document = {
+//     content: text,
+//     type: 'PLAIN_TEXT',
+// };
+app.post('/test', async (req, res) => {
+    if (!req.files || !req.files.xmlupload || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    } else if (!req.files.xmlupload.mimetype.includes("xml")) {
+        return res.status(400).send('Incorrect filetype')
+    }
+    const f = req.files.xmlupload
+    console.log(f.mimetype, f.size)
+    console.log(await xmlParser.parseXML(f.data.toString()))
+    // parser.parse(f.data)
+    res.send('yeet')
+})
+
+async function sentimentAnalysis(parsedXML) {
+
+
+    const sentencesArray = []
+    parsedXML.forEach((userEntry) => {
+        userEntry.forEach((textObject) => {
+            sentencesArray.push(textObject.body)
+        })
+    })
+    const combinedSentences = sentencesArray.join(". ")
+
+    const [result] = await client.analyzeSentiment({
+        document: {
+            content: combinedSentences,
+            type: 'PLAIN_TEXT'
+        }
+    });
+    const sentiments = result.sentences.map((result) => {
+        return result.sentiment.score;
+    })
+    let i = 0;
+    parsedXML.forEach((userEntry) => {
+        userEntry.forEach((textObject) => {
+            textObject["score"] = sentiments[i]
+            i++;
+        })
+    })
+}
 
 
 app.get('/upload', async (req, res) => {
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
-    }
+    const parsedXML = [
+        [{
+                "body": "text 1",
+                "date": "asdasdasasdsa"
+            },
+            {
+                "body": "text number two",
+                "date": "asdasdasasdsa"
+            },
+            {
+                "body": "I hate you",
+                "date": "asdasdasasdsa"
+            }
+        ],
+        [{
+                "body": "i like you and i want to go home",
+                "date": "asdasdasasdsa"
+            },
+            {
+                "body": "yeet is a nice word plus ect",
+                "date": "asdasdasasdsa"
+            },
+            {
+                "body": "hello this is a test",
+                "date": "asdasdasasdsa"
+            }
+        ]
+    ]
 
-    const parsedXML = await parseXML(req.files.xmlupload)
-
-
-
-    req.files.
-    const
-    // res.redirect("/index.html")
-
-    const [result] = await client.analyzeSentiment({
-        document: document
-    });
-    console.log(result)
-    result.sentences.forEach((obj) => {
-        console.log(obj.sentiment, obj.text)
-    })
+    sentimentAnalysis(parsedXML)
+    console.log(parsedXML);
     res.send("hello")
-    // const sentiment = result.sentences;
-    // console.log(`Text: ${text}`);
-    // console.log(`Sentiment score: ${sentiment.score}`);
-    // console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
 });
-
-// async function parseXML(f) {
-    
-// }
 
 app.listen(port, () => console.log(`Started at port ${port}`))
