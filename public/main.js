05b8a202982463a477e8749d548f60e3ed630d64
tylermcdonald <40723631+tylermcdonald@users.noreply.github.com>
2020-03-01 00:19:56 -0600

    Still having trouble with date formatting

--- public/main.js
+++ public/main.js
@@ -28,7 +28,6 @@ function lineGraph(jsonData) {
     let config = {
         type: 'line',
         data: {
-            labels: [],
             datasets: [
 
             ]
@@ -46,7 +45,7 @@ function lineGraph(jsonData) {
                 intersect: true,
                 callbacks: {
                     label: function(tooltipItems, data){
-                        var prefix = "NOT";
+                        var prefix = "";
                         return prefix;
                     }
                 }
@@ -56,31 +55,36 @@ function lineGraph(jsonData) {
                 intersect: true
             },
             scales: {
-                x: {
-                    display: true,
+                xAxes : [{
+                    type: 'time',
+                    distribution: 'linear',
+                    time:{
+                        unit: 'day',
+                        //tooltipFormat: 'MMMM Do YYYY, h:mm:ss a'
+                    },
+                    scaleLabel:{
+                        display: true,
+                        labelString: 'Date'
+                    }
+                }],
+                yAxes: [{
                     scaleLabel: {
                         display: true,
-                        labelString: 'Month'
+                        labelString: 'value'
                     }
-                },
-                y: {
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Value'
-                    }
-                }
+                }]
             }
         }
     }
 
+    const DATE_FORMAT = "MMMM Do YYYY, h:mm:ss";
     const colorList = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#000000'];
 
     let dateArr = []
+    
     jsonData.forEach(userData => {
         userData.forEach(message => {
             const curDate = Math.floor(parseInt(message.date.trim()) / 1000);
-
             if (!(dateArr.includes(curDate))) {
                 dateArr.push(curDate)
             }
@@ -88,29 +92,27 @@ function lineGraph(jsonData) {
     })
     dateArr.sort();
 
-    dateArr.forEach(date => {
-        config.data.labels.push(date.toString())
-    })
-
     let userCount = 0;
     let dateToMessageMapArr = []
     jsonData.forEach(userData => {
         let sentimentDateMap = {};
         dateToMessageMapArr.push({});
         userData.forEach(message => {
-            const curDate = Math.floor(message.date / 1000);
+            const curDate = Math.floor(parseInt(message.date.trim()) / 1000);
             const curSentiment = message.score;
-
-            if (curDate in dateToMessageMapArr[userCount]) {
-                const newDateString = dateToMessageMapArr[userCount][curDate].concat("...").concat(message.body);
-                dateToMessageMapArr[userCount][curDate] = newDateString;
+            let curFormattedDate = moment.unix(curDate, DATE_FORMAT);
+            
+            console.log(curFormattedDate)
+            if (curFormattedDate in dateToMessageMapArr[userCount]) {
+                const newDateString = dateToMessageMapArr[userCount][curFormattedDate].concat("...").concat(message.body);
+                dateToMessageMapArr[userCount][curFormattedDate] = newDateString;
             } else {
-                dateToMessageMapArr[userCount][curDate] = message.body;
+                dateToMessageMapArr[userCount][curFormattedDate] = message.body;
             }
 
-            if (dateToMessageMapArr[userCount][curDate].length > 100) {
-                dateToMessageMapArr[userCount][curDate] = dateToMessageMapArr[userCount][curDate].substring(0, 100);
-                dateToMessageMapArr[userCount][curDate] = dateToMessageMapArr[userCount][curDate].concat("...");
+            if (dateToMessageMapArr[userCount][curFormattedDate].length > 100) {
+                dateToMessageMapArr[userCount][curFormattedDate] = dateToMessageMapArr[userCount][curFormattedDate].substring(0, 100);
+                dateToMessageMapArr[userCount][curFormattedDate] = dateToMessageMapArr[userCount][curFormattedDate].concat("...");
             }
 
             if (curDate in sentimentDateMap) {
@@ -135,34 +137,46 @@ function lineGraph(jsonData) {
             fill: false,
             spanGaps: true
         }
-        const dates = Object.keys(sentimentDateMap);
-        let curDateIndex = 0;
-        let curDate = dateArr[curDateIndex];
-        dates.forEach(date => {
-            while (dateArr[curDateIndex] < date) {
-                //const sentimentAverage = sentimentDateMap[date].sentimentSum/sentimentDateMap[date].count;
-                newSentiment.data.push(null);
-                curDateIndex += 1
+
+        dateArr.forEach(date => {
+            let sentimentAverage = null;
+            if(date in sentimentDateMap){
+                sentimentAverage = sentimentDateMap[date].sentimentSum / sentimentDateMap[date].count;
             }
-            const sentimentAverage = sentimentDateMap[date].sentimentSum / sentimentDateMap[date].count;
-            newSentiment.data.push(sentimentAverage);
-            curDateIndex += 1
-        });
+
+            newSentiment.data.push({x: new Date(moment.unix(date, DATE_FORMAT)), y: sentimentAverage});
+        })
+
         config.data.datasets.push(newSentiment);
         userCount += 1
     });
+    console.log(config);
     config.options.tooltips.callbacks.label = function (tooltipItems, data) {
-        
-        let hoveredDate = parseInt(tooltipItems.label);
-        let yValue = tooltipItems.value.substring(0, 4)
-        let user = tooltipItems.datasetIndex
+        const hoveredDate = tooltipItems.label;
+
+        const hoveredUnix = moment.unix(hoveredDate, DATE_FORMAT)
+        console.log(tooltipItems)
+        console.log(dateToMessageMapArr[0])
+        const yValue = tooltipItems.value.substring(0, 4)
+        const user = tooltipItems.datasetIndex
 
         let message = yValue.concat(" --- ");
-        message = message.concat(dateToMessageMapArr[user][hoveredDate] || "N/A")       
+        message = message.concat(dateToMessageMapArr[user][hoveredUnix] || "N/A")       
         return message;
     };
     
     let ctx = document.getElementById('canvas').getContext('2d');
     window.myLine = new Chart(ctx, config);
 
+}
+
+function convertDateFormat(date){
+    let dateC = date.substring(4, date.length-9);
+    dateC = dateC.slice(0, 6)+","+dateC.slice(6);
+    if(dateC.charAt(4) == '0'){
+        dateC = dateC.slice(0, 3) + dateC.slice(4);
+    }
+    let to_ret = [dateC.concat(" am"), dateC.concat(" pm")]
+    return to_ret;
+    //dateC = dateC.replace(',','');
 }
